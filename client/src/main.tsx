84e382eff0f4e9bf0e9f9550c1bf5d076a792384
Ahmed Badour <ahmed.badour2005@gmail.com>
2026-05-16 13:21:43 +0300

    connect frontend to production backend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App'
 import './i18n'
 import './index.css'
 import { initTheme } from './store/themeStore'
+
+// Configure Axios base URL globally for production
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL
+}
 
 initTheme()
 
